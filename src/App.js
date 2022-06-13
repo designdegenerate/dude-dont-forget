ec5e8748b8161eedb3c0c4abdd4cd8827c3e80a1
nextcode1 <stargateblk@gmail.com>
2022-06-13 14:21:31 +0200

    appState created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import MessageBox from "./components/MessageBox";
 
 function App() {
   return (
     <div className="App">
+      <MessageBox />
       <h1>Dude Don't Forget</h1>
       <div className="AppNav">
         <NavLink to="/home">Home</NavLink>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import appStateReducer from "./appState/slice";
 import userReducer from "./user/slice";
 
 export default configureStore({
   reducer: {
+    appState: appStateReducer,
     user: userReducer,
   },
 });
