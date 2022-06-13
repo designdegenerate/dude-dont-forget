c2e9218d3db45afad17306186dc01437a5cdb51f
git-meaux <maurice.vd.erve@gmail.com>
2022-06-13 17:21:43 +0200

    basic design

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  /* </React.StrictMode> */
   document.getElementById("root")
 );
 
---
+++ src/pages/ListPage/ListPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ListPage() {
+  return <div>
+    <h2>All the important stuff</h2>
+    <div>
+        <p>list of stuff</p>
+    </div>
+  </div>;
+}
