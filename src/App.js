21ee9d40f2c617ad29bbd505fe7477e07b75f5fb
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-06-14 15:24:08 +0200

    fix duplicate imports

--- src/App.js
+++ src/App.js
@@ -2,33 +2,22 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-
-import { Names, partnersLoading } from "./store/partner/slice";
-import { getSomeNames } from "./store/partner/actions";
-import { selectPartners } from "./store/partner/selectors";
-
+import { useEffect} from "react";
 import HomePage from "./pages/HomePage";
 import MessageBox from "./components/MessageBox";
 import Login from "./pages/Login/Login";
 import { logOut } from "./store/user/slice";
-import { useDispatch, useSelector } from "react-redux";
 import { selectToken } from "./store/user/selectors";
 import SignUp from "./pages/SignUp/SignUp";
-import { useEffect } from "react";
 import { getUserWithStoredToken } from "./store/user/actions";
 
 function App() {
   const dispatch = useDispatch();
 
-  const isPartnerLoading = useSelector(selectPartners);
-
-
   const token = useSelector(selectToken);
 
   useEffect(() => {
     dispatch(getUserWithStoredToken());
-    dispatch(getSomeNames);
   }, [dispatch]);
 
 
