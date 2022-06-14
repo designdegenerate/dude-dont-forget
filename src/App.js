d2b9ad1fb0dee54fae44165797c31353f0f5f7ac
w.alvesnl <w.alvesnl@gmail.com>
2022-06-14 12:39:44 +0200

    login page and actions

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import { NavLink } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MessageBox from "./components/MessageBox";
 import Login from "./pages/Login/Login";
+import { useDispatch } from "react-redux";
+import { getUserWithStoredToken } from "./store/user/actions";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <MessageBox />
