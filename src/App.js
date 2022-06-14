c6029a3e2ab3156c58b98b7eca1f6e5a9ccd9f4d
w.alvesnl <w.alvesnl@gmail.com>
2022-06-14 13:59:55 +0200

    login and signup forms

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import HomePage from "./pages/HomePage";
 import MessageBox from "./components/MessageBox";
 import Login from "./pages/Login/Login";
 import { logOut } from "./store/user/slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectToken } from "./store/user/selectors";
+import SignUp from "./pages/SignUp/SignUp";
+import { useEffect } from "react";
+import { getUserWithStoredToken } from "./store/user/actions";
 
 function App() {
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
 
   return (
     <div className="App">
@@ -19,14 +28,23 @@ function App() {
         <NavLink to="/home">Home</NavLink>
         <br />
         <br />
-        <NavLink to="/login">Login</NavLink>
-        <br />
-        <br />
-        <button onClick={() => dispatch(logOut())}>Logout</button>
+        {!token ? (
+          <div>
+            <NavLink to="/signup">Sign up</NavLink>
+            <br />
+            <br />
+            <NavLink to="/login">Login</NavLink>
+            <br />
+            <br />
+          </div>
+        ) : (
+          <button onClick={() => dispatch(logOut())}>Logout</button>
+        )}
       </div>
       <Routes>
         <Route exact path="/home" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
