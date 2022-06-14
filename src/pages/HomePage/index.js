92c9c2029cc31b1a8862850fb5a49cff2171d948
normanvisser <normanvisser88@gmail.com>
2022-06-14 15:08:18 +0200

    Made list items iclickable, added add partner form

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,33 +1,96 @@
-import React from "react";
-
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./styles.css";
+// import EventCard from "../../components/EventCard";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const [showForm, setShowForm] = useState(false);
+  const [partnerName, setPartnerName] = useState("");
+  const [toggleEventForm, setToggleEventForm] = useState(false);
+
+  console.log(partnerName);
+
   return (
     <div className="main-container">
       <div>
         <div className="names">
           <ul>
-            <li>Katie</li>
-            <li>Alice</li>
-            <li>Jessica</li>
+            <li className="names-button" onClick={() => console.log("Katie")}>
+              Katie
+            </li>
+            <li className="names-button" onClick={() => console.log("Alice")}>
+              Alice
+            </li>
+            <li className="names-button" onClick={() => console.log("Jessica")}>
+              Jessica
+            </li>
           </ul>
-          <button>Add</button>
+          <div className="add-name">
+            {showForm ? (
+              <div className="add-name-form">
+                <form>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    open={false}
+                    className="form-input"
+                    onChange={(e) => setPartnerName(e.target.value)}
+                  />
+                </form>
+                <button
+                  className="add-name-button"
+                  onClick={() => {
+                    // dispatch(addNewPartner(partnerName));
+                    setShowForm(false);
+                  }}
+                >
+                  Add
+                </button>
+              </div>
+            ) : null}
+            <button
+              onClick={() => {
+                setShowForm(!showForm);
+              }}
+              className="toggle-add-partner-form"
+            >
+              {showForm ? "×" : "+"}
+            </button>
+          </div>
         </div>
         <ul className="details-bar">
-          <li>Events</li>
-          <li>Facts</li>
+          <li
+            className="events-facts-button"
+            onClick={() => console.log("Events")}
+          >
+            Events
+          </li>
+          <li
+            className="events-facts-button"
+            onClick={() => console.log("Facts")}
+          >
+            Facts
+          </li>
         </ul>
         <div className="events">
-          <button>+</button>
           <div className="event-list">
+            {/* <EventCard /> */}
+
             <div className="event-card">Card</div>
             <div className="event-card">Card</div>
             <div className="event-card">Card</div>
           </div>
         </div>
+        <button
+          className="add-event-button"
+          onClick={() => {
+            setToggleEventForm(!toggleEventForm);
+          }}
+        >
+          +
+        </button>
       </div>
-      Welcome to HomePage
     </div>
   );
 }
